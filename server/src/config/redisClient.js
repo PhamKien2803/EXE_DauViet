e562b0120233e167b4551b14bd0b7963dcb13334
Hieubthe173123 <trunghieubui866@gmail.com>
2025-06-28 00:11:21 +0700

    redis

--- server/src/config/redisClient.js
+++ server/src/config/redisClient.js
@@ -1,10 +1,12 @@
 const redis = require("redis");
 
 const redisClient = redis.createClient({
-  socket: {
-    host: "127.0.0.1",
-    port: 6379,
-  },
+   username: 'default',
+        password: process.env.REDIS_PASSWORD,
+        socket: {
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT,
+        }
 });
 
 redisClient.on("error", (err) => {
